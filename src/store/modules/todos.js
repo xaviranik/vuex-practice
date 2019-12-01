f9f8a81a5f73f6ede41b2cdeb9fda5f0e192d8f1
xavir <xaviranik.me@gmail.com>
2019-12-01 21:02:28 +0600

    Added: fetchTodos mapActions

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,25 +1,27 @@
-// import axios from 'axios'
+import axios from "axios"
+
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo 1'
-        },
-        {
-            id: 2,
-            title: 'Todo 2'
-        },
-    ]
+    todos: []
 }
 
 const getters = {
     allTodos: (state) => state.todos
 }
 
-const actions = {}
+const actions = {
+    async fetchTodos({ commit }) {
+        const url = "https://jsonplaceholder.typicode.com/todos";
 
-const mutations = {}
+        const response = await axios.get(url)
+
+        commit('setTodos', response.data)
+    }
+}
+
+const mutations = {
+    setTodos: (state, todos) => {state.todos = todos}
+}
 
 export default {
     state,
